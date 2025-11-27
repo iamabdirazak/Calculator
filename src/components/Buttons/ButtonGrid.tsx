26699f8634cc6f4e5af8d14a1b11110cb7161da9
iamabdirazak <a.razakmubarak@icloud.com>
2025-11-27 20:50:29 +0300

    improve grid layout

--- src/components/Buttons/ButtonGrid.tsx
+++ src/components/Buttons/ButtonGrid.tsx
@@ -1,6 +1,6 @@
 import Button from "./Button";
 import "./ButtonGrid.css";
-import type Operator from ".../types/calculator";
+import type {Operator} from "/Users/abdirazak/Developer/Calculator/src/types/calculator";
 
 interface Props {
   onDigit: (d: string) => void;
@@ -21,27 +21,29 @@ export default function ButtonGrid({
 }: Props) {
   return (
     <div className="grid">
-      <Button label="C" variant="action" onClick={onClear} />
-      <Button label="DEL" variant="action" onClick={onDelete} />
-      <Button label="." variant="action" onClick={onDot} />
-      <Button label="÷" variant="operator" onClick={() => onOperator("/")} />
+      
 
       <Button label="7" onClick={() => onDigit("7")} />
       <Button label="8" onClick={() => onDigit("8")} />
       <Button label="9" onClick={() => onDigit("9")} />
-      <Button label="×" variant="operator" onClick={() => onOperator("*")} />
+      <Button label="÷" variant="operator" onClick={() => onOperator("/")} />
 
       <Button label="4" onClick={() => onDigit("4")} />
       <Button label="5" onClick={() => onDigit("5")} />
       <Button label="6" onClick={() => onDigit("6")} />
-      <Button label="−" variant="operator" onClick={() => onOperator("-")} />
+      <Button label="×" variant="operator" onClick={() => onOperator("*")} />
 
       <Button label="1" onClick={() => onDigit("1")} />
       <Button label="2" onClick={() => onDigit("2")} />
       <Button label="3" onClick={() => onDigit("3")} />
+      <Button label="−" variant="operator" onClick={() => onOperator("-")} />
+
+      <Button label="DEL" variant="action" onClick={onDelete} />
+      <Button label="0" onClick={() => onDigit("0")} />
+      <Button label="." variant="action" onClick={onDot} />
       <Button label="+" variant="operator" onClick={() => onOperator("+")} />
 
-      <Button label="0" onClick={() => onDigit("0")} />
+      <Button label="AC" variant="action" onClick={onClear} />
       <Button label="=" variant="operator" onClick={onEqual} />
     </div>
   );
